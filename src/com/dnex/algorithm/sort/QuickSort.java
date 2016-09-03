1f584f89e088e34d991c273ef8b5de6f41619f5e
Ajay <adonode@Suparnas-MacBook-Pro.local>
2016-09-03 11:29:05 -0700

    [Ajay Donode] Added QuickSort code

--- src/com/dnex/algorithm/sort/QuickSort.java
+++ src/com/dnex/algorithm/sort/QuickSort.java
@@ -6,17 +6,18 @@ public class QuickSort {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		 int[] unsorted = {32, 39,21, 45, 23, 3};
-	        quickSort(unsorted);
-	      
-	        int[] test = { 5, 3, 2, 1};
-	        quickSort(test);
 
+		int[] unsorted = { 32, 39, 21, 45, 23, 3 };
+		quickSort(unsorted);
+
+		int[] test = { 5, 3, 2, 1 };
+		quickSort(test);
+ 
 	}
 
 	private static void quickSort(int[] unsorted) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
