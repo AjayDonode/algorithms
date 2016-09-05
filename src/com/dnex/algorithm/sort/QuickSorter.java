3eb9208d3ca57a1deeb2214d4a9e7c20b4f83529
Ajay <ajaykumar_donode@gap.com>
2016-09-05 00:00:34 -0700

    [Ajay Donode] Otimized code for Quick Sort

--- src/com/dnex/algorithm/sort/QuickSorter.java
+++ src/com/dnex/algorithm/sort/QuickSorter.java
@@ -1,57 +1,50 @@
 package com.dnex.algorithm.sort;
 
-import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
-public class QuickSorter
-{
-   public static void swap (int A[], int x, int y)
-   {
-      int temp = A[x];
-      A[x] = A[y];
-      A[y] = temp;
-   }
+public class QuickSorter {
 
-   // Reorganizes the given list so all elements less than the first are 
-   // before it and all greater elements are after it.                   
-   public static int partition(int A[], int f, int l)
-   {
-      int pivot = A[f];
-      while (f < l)
-      {
-         if (A[f] == pivot || A[l] == pivot) 
-         {
-//            System.out.println("Only distinct integers allowed - C321");
-//            System.out.println("students should ignore this if statement");
-           // System.out.exit(0);
-         }
-         while (A[f] < pivot) f++;
-         while (A[l] > pivot) l--;
-         swap (A, f, l);
-      }
-      return f;
-   }
+	public static int partition(int[] list, int low, int high) {
+		int pivot = list[low];
+		while (low < high) {
+			if (list[high] >= pivot) {
+				high--;
+			}
+			list[low] = list[high];
+			
+			if (list[low] <= pivot) {
+				low++;
+			}
+		
+			list[high] = list[low];
+		}
 
-   public static void QuickSorter(int A[], int f, int l)
-   {
-      if (f >= l) return;
-      int pivot_index = partition(A, f, l);
-      System.out.println("Pivot"+pivot_index);
-      QuickSorter(A, f, pivot_index);
-      QuickSorter(A, pivot_index+1, l);
-   }
+		list[low] = pivot;
+		return low;
 
-   // Usage: java QuickSort [integer] ...
-   // All integers must be distinct
-   public static void main(String argv[])
-   {
-//      int A[] = new int[argv.length];
-//      for (int i=0 ; i < argv.length ; i++)
-	   int A[]  = { 32, 39, 21, 45, 23, 3 };
+	}
 
-      QuickSorter(A, 0, A.length-1);
+	public static void quicksort(int[] list, int low, int high) {
+		if (low < high) {
+			int mid = partition(list, low, high);
+			quicksort(list, low, mid - 1);
+			quicksort(list, mid + 1, high);
+		}
+	}
 
-      for (int i=0 ; i < A.length ; i++) System.out.print(A[i] + " ");
-      System.out.println();
-   }
+	// Usage: java QuickSort [integer] ...
+	// All integers must be distinct
+	public static void main(String argv[]) {
+		// int A[] = new int[argv.length];
+		// for (int i=0 ; i < argv.length ; i++)
+		int A[] = { 32, 39, 21, 45, 23, 3 };
+
+		int B[] = { 1, 4, 2, 5, 3, 8 };
+
+		quicksort(A, 0, A.length - 1);
+		quicksort(B, 0, B.length - 1);
+
+		System.out.println(Arrays.toString(A));
+		System.out.println(Arrays.toString(B));
+	}
 }
