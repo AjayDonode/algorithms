c22145b7c2e3562acd1f3de3b012f46cd501aa30
“Ajay <ajay.donode@ge.com>
2017-02-15 14:37:23 -0800

    RemoveAdjuscentDuplicates Code [Asked in Oracle]

--- src/com/dnex/algorithm/string/RemoveAdjuscentDuplicates.java
+++ src/com/dnex/algorithm/string/RemoveAdjuscentDuplicates.java
@@ -1,32 +1,25 @@
 package com.dnex.algorithm.string;
 
 /**
- * Created by 502664102 on 2/15/17.
+ * Created by Ajay Donode on 2/15/17.
  */
 public class RemoveAdjuscentDuplicates {
     public static void main(String args[]){
-
         System.out.println("Hello");
         String inputString =  "aabbcdde";
         System.out.println("Output String "+ filterString(inputString, null));
     }
 
     private static String filterString(String inputString, String lastElement) {
-        if(inputString == null) return null ;
-        if(inputString.length() ==1 ) return inputString;
-
+        if(inputString == null) return null;
+        if(inputString.length() == 1 ) return inputString;
             String firstElement = inputString.substring(0,1);
             String remainingString = inputString.substring(1, inputString.length());
-
             StringBuffer sb = new StringBuffer(); //First element
         if(!firstElement.equals(lastElement)) {
             sb.append(firstElement);
             lastElement = firstElement;
-
         }
-
-
-
         return sb.append(filterString(remainingString, lastElement)).toString();
 
     }
